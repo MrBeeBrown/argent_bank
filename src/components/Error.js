b53ed162ff6574daec69582a634bd767e7de58b9
beebrown <bruno.grosdesirs@outlook.fr>
2024-08-17 15:44:43 +0200

    update error

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,17 +1,11 @@
-import { Link } from 'react-router-dom'
-import logo from '../assets/argentBankLogo.png'
-import user from '../assets/circle-user-solid.svg'
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 const Error = () => {
   return (
     <div>
-      <header>
-        <Link to="/"><img src={logo} alt="Argent Bank logo" /></Link>
-        <div className="sign__in">
-          <img src={user} alt="Sign In" />
-          <Link to="/login"><p>Sign In</p></Link>
-        </div>
-      </header>
+      <Header />
       <main>
         <div className='error'>
           <h1>Error 404</h1>
@@ -19,9 +13,7 @@ const Error = () => {
           <p>Please check the URL in the address bar and try again.</p>
         </div>
       </main>
-      <footer>
-        <p>Copyright {new Date().getFullYear()} Argent Bank</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
